7d9a05eb0a1c7cdb5acc294f1d4c87656fb4863d
Maggi <yuen.maggi@gmail.com>
2025-05-26 10:50:46 +0200

    2-colums layout

--- mk_erin.php
+++ mk_erin.php
@@ -1,12 +1,18 @@
 <form method="POST">
-    <label for="mkTodo" name="lbl_mk_mkTodo">Erinnerung erstellen</label>
-    <input type="text" id="mkTodo" name="frm_mktodo">
+    <div class="col_2">
+        <label for="mkTodo" name="lbl_mk_mkTodo">Hier eine neue Erinnerung erstellen</label>
+        <input type="text" id="mkTodo" name="frm_mktodo">
+    </div>
 
-    <label for="datetime" name="lbl_datetime">Passwort</label>
-    <input type="datetime" name="frm_datetime" id="datetime">
+    <div class="col_2">
+        <label for="datetime" name="lbl_datetime">Passwort</label>
+        <input type="datetime" name="frm_datetime" id="datetime">
+    </div>
 
-    <label for="u_id" name=""lbl_u_id>Benutzer ID:</label>
-    <input type="text" name="frm_u_id" id="u_id">
+    <div class="col_2">
+        <label for="u_id" name="" lbl_u_id>Benutzer ID:</label>
+        <input type="text" name="frm_u_id" id="u_id">
+    </div>
 
     <input type="submit" value="Erstellen">
 </form>
