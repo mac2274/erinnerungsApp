49b88368d16d9424aa1cff40dd7beed94997790f
Maggi <yuen.maggi@gmail.com>
2025-05-28 09:16:15 +0200

    prepared.php

---
+++ config/prepared.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM erinnerung WHERE id=?";
+
+    if ($stmt = mysqli->prepare($sql)) {
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        if (!$stmt->errno) {
+            echo 'Abfrage war fehlerhaft.';
+        }
+        $result = $stmt->get_result;
+        while ($row = $result->fetch_assoc()) {
+            echo '<b><a href="?id=' . $row['id'].'"></a></b>'.$row['id'].'<br>';
+        }
+    }
+}
