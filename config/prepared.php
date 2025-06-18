e214323797f0430ef6e4989d836c023cc2478862
Maggi <yuen.maggi@gmail.com>
2025-06-18 14:19:09 +0200

    add comments/questions about possibility of code

--- config/prepared.php
+++ config/prepared.php
@@ -17,8 +17,13 @@ if (isset($_GET['id'])) {
 
         $result = $stmt->get_result();
         while ($row = $result->fetch_assoc()) {
-            echo '<hr><b><a href="?id=' . $row['id'].'">#' . $row['id'].'</a>: '.htmlspecialchars($row['value']).'</b>('.$row['deadline'].')<br>';
-            echo '<p>'.($row['description']).'</p>';
+            echo '<hr><b><a href="?id='.htmlspecialchars($row['id']).'">#' . $row['id'].'</a>: '.htmlspecialchars($row['value']).'</b> -'.htmlspecialchars($row['u_id']). '('.htmlspecialchars($row['deadline']).')<br>';
+            echo '<p>'.(htmlspecialchars($row['description'])).'-'.htmlspecialchars($row['status']).'</p>';
         }
     }
 }
+
+
+// ich möchte statt u_id die Person(name) zeigen
+// genau so mit status, hier gerne den value aus der status-tabelle
+// geht das, wenn man eig hier aus dem row
