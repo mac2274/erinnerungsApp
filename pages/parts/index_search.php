37f10918f23e866239a1c674aab83ab15486e728
Maggi <yuen.maggi@gmail.com>
2025-06-21 12:01:06 +0200

    css

--- pages/parts/index_search.php
+++ pages/parts/index_search.php
@@ -1,17 +1,15 @@
 <div class="wrapper padding-top-5">
 
-<h3>Suche nach Erinnerungen</h3>
+    <h3>Suche nach Erinnerungen</h3>
     <form method="POST">
         <div class="col_2 ">
             <label for="search_id">ID-Suche:</label>
             <input type="text" name="search_id" id="search_id">
-            <input type="submit" name="search_id_submit" value="Suchen">
         </div>
 
-        <div class="col_2">
-            <label for="search_erin">Erinnerungssuche:</label>
-            <input type="text" name="search_erin" id="search_erin">
-            <input type="submit" name="search_erin_submit" value="Suchen">
-        </div>
+        <label for="search_erin">Erinnerungssuche:</label>
+        <input type="text" name="search_erin" id="search_erin">
+
+        <input type="submit" name="search_submit" value="Suchen">
     </form>
 </div>
