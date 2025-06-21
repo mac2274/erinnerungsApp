b0985623ee8cd2db39a632ee1731369458e795c7
Maggi <yuen.maggi@gmail.com>
2025-06-21 11:09:17 +0200

    expamd search by search_id and search_erin

--- config/search.php
+++ config/search.php
@@ -1,24 +1,29 @@
 <?php
 if (isset($_POST['search_id']) || isset($_POST['search_erin'])) { // aus dem Formular search.php
     $searchID = $_POST['search_id'];
+    $searchErin = $_POST['search_erin'];
 
-    $sql = "SELECT * from erinnerung WHERE id=?";
+    $sql = "SELECT * from erinnerung WHERE id=? OR value=?";
 
     $stmt3 = $mysqli->prepare($sql);
-    $stmt3->bind_param("i", $searchID);
+    $stmt3->bind_param("is", $searchID, $searchErin);
     $stmt3->execute();
 
     $result = $stmt3->get_result();
 
     if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
         echo "<h3>Erinnerung gefunden:</h3>";
-        echo "ID: " . htmlspecialchars($row['id']) . "<br>";
-        echo "Inhalt: " . htmlspecialchars($row['value']) . "<br>";
-        echo "Fällig am: " . htmlspecialchars($row['deadline']) . "<br>";
-        echo "Beschreibung: " . htmlspecialchars($row['description']) . "<br>";
+
+        while ($row = $result->fetch_assoc()) {
+            
+            echo "ID: " . htmlspecialchars($row['id']) . "<br>";
+            echo "Inhalt: " . htmlspecialchars($row['value']) . "<br>";
+            echo "Fällig am: " . htmlspecialchars($row['deadline']) . "<br>";
+            echo "Beschreibung: " . htmlspecialchars($row['description']) . "<br>";
+            echo '<hr>';
+        }
     } else {
-        echo '0 results';
+        echo 'Keine passende Erinnerung gefunden aus: "' .htmlspecialchars($_POST[('search_erin')]).'"';
     }
     //$stmt3->close(); 
 }
