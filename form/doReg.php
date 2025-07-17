e08bc001ff449ae07722ce53ccef8d010ef78c25
Maggi <yuen.maggi@gmail.com>
2025-07-17 13:20:50 +0200

    adjust paths

--- form/doReg.php
+++ form/doReg.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'config/lib.php';
+require_once '../config/lib.php';
 
 if (isset($_SERVER['REQUEST_METHOD']) == 'POST' && isset($_POST['reg_submit'])){
     regUser($name, $email, $hashedPwd);
     // echo 'Registered!';
-    require 'home.html';
+    require '../pages/home.html';
 } else {
     echo 'Ein Fehler ist aufgetreten.';
 }
