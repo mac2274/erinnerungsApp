08746527f59cce82d8c744bf70a36bcf6fa05070
Maggi <yuen.maggi@gmail.com>
2025-07-18 08:46:54 +0200

    login function

--- config/lib.php
+++ config/lib.php
@@ -3,23 +3,40 @@ session_start();
 
 require_once 'config.db.php';
 
-// variables
-$name = $_POST['reg_name'];
-$email = $_POST['reg_email'];
-$hashedPwd = password_hash($_POST['reg_pwd'], PASSWORD_DEFAULT);
 
 function regUser($name, $email, $hashedPwd)
-{   
+{
     global $mysqli;
+    // variables
+    $name = $_POST['reg_name'];
+    $email = $_POST['reg_email'];
+    $hashedPwd = password_hash($_POST['reg_pwd'], PASSWORD_DEFAULT);
+    
     $sql = "INSERT INTO user (name, email, password) VALUE (?,?,?);";
+    $stmt = $mysqli->prepare($sql);
+    if (!$stmt) {
+        throw new Exception($mysqli->error);
+    }
+    $stmt->bind_param('sss', $name, $email, $hashedPwd);
+    if (!$stmt->execute()) {
+        throw new Exception($stmt->error);
+    }
+    return $stmt->affected_rows;
+}
+
+function loginUser()
+{
+    global $mysqli;
+    $email = $_POST['login_email'];
+    $sql = "SELECT FROM user (email) VALUE (?);";
     $stmt = $mysqli->prepare($sql);
     if (!$stmt){
         throw new Exception($mysqli->error);
     }
-    $stmt->bind_param('sss', $name, $email, $hashedPwd);
+    $stmt->bind_param('s', $email);
     if (!$stmt->execute()){
         throw new Exception($stmt->error);
     }
-    
-    return $stmt->affected_rows;
+    return $stmt->get_result();
 }
+;
--- form/doReg.php
+++ form/doReg.php
@@ -1,8 +1,11 @@
 <?php
+
 require_once '../config/lib.php';
 
 if (isset($_SERVER['REQUEST_METHOD']) == 'POST' && isset($_POST['reg_submit'])){
     regUser($name, $email, $hashedPwd);
+    $_SESSION['name'] = $name;
+    echo $name;
     // echo 'Registered!';
     require '../pages/home.html';
 } else {
